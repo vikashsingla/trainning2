b2768a01f722bfb44be2804668bf9b339389f42d
Vikash <rockysingla1@gmail.com>
2013-08-30 11:25:07 +0530

    new commit

---
+++ canvas web page.js
@@ -0,0 +1,14 @@
+function button()
+{
+
+    var c=document.getElementById("demo").src="canvas.html";
+}
+function button2()
+{
+    var c=document.getElementById("demo").src="wabpage.html";
+
+}
+function i()
+{
+    var c=document.getElementById("demo1").src="file:///home/manusis/Pictures/images1.jpg";
+}
